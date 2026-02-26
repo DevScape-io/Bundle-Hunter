c4dbfa6acf1ee56498b4d108c755c366ddefc7af
Parker Howell <24279223+pdhowell@users.noreply.github.com>
2026-02-26 09:54:59 -0600

    Initial Commit

---
+++ Bundle HunterTests/Bundle_HunterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bundle_HunterTests.swift
+//  Bundle HunterTests
+//
+//  Created by Parker Howell on 2/26/26.
+//
+
+import Testing
+@testable import Bundle_Hunter
+
+struct Bundle_HunterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Bundle HunterUITests/Bundle_HunterUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Bundle_HunterUITestsLaunchTests.swift
+//  Bundle HunterUITests
+//
+//  Created by Parker Howell on 2/26/26.
+//
+
+import XCTest
+
+final class Bundle_HunterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
